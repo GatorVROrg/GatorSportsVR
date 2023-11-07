7feaeba675d938df3297c4078046faebe22c1533
Keith Khadar <113483023+Keith-Khadar@users.noreply.github.com>
2023-11-07 12:55:54 -0500

    Within cells

--- Assets/PlayerSpawn.cs
+++ Assets/PlayerSpawn.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerSpawn : MonoBehaviour
+public class PlayerSpawn : NetworkBehaviour
 {
     public Transform spawnTransform;
     // Start is called before the first frame update
-    void Start()
+
+    public override void OnNetworkSpawn()
     {
-        transform.position = spawnTransform.position;   
+        transform.position = spawnTransform.position;
     }
 }
