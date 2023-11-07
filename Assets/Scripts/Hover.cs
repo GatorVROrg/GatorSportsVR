411472072e0a578cc7f209f9f46a3a96036fef3f
Bruno <bruno.v.dede@gmail.com>
2023-11-07 12:40:14 -0500

    Updated Hover script

--- Assets/Scripts/Hover.cs
+++ Assets/Scripts/Hover.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Hover : MonoBehaviour
 {
+    float degreeValue = 0;
+    float magnitude = 14;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,13 @@ public class Hover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(0, Mathf.Cos(degreeValue * Mathf.Deg2Rad) * magnitude * Time.deltaTime, 0);
+
+        degreeValue += 1;
+
+        if (degreeValue >= 360)
+        {
+            degreeValue = 0;
+        }
     }
 }
