5b366736ed4cfac014687b81b7b1d1abbd38c8c3
Keith Khadar <113483023+Keith-Khadar@users.noreply.github.com>
2023-11-07 12:53:37 -0500

    Interlinked

--- Assets/PlayerSpawn.cs
+++ Assets/PlayerSpawn.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class PlayerSpawn : MonoBehaviour
 {
+    public Transform spawnTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        transform.position = spawnTransform.position;   
     }
 }
